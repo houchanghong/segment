ee882c0ccb00e9b15a098c1af5e0a5103ff39f3d
houchanghong <houchanghong@me.com>
2014-12-12 15:24:04 +0800

    Initial Commit

---
+++ segment/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  segment
+//
+//  Created by Mac on 14/12/12.
+//  Copyright (c) 2014年 Mac. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ segment/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  segment
+//
+//  Created by Mac on 14/12/12.
+//  Copyright (c) 2014年 Mac. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
